b71938bbc59f054fc7e0dd1324d52d6f97f67fd0
Matheus <matheussatonino9@gmail.com>
2025-04-10 13:51:03 -0300

    Correção - "Digite seu nome" estava sendo pulado do código.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,8 +7,9 @@ public class ContaTerminal {
 
         System.out.println("----------------------------------------------");
 
-        System.out.print("Digite o número de conta:");
+        System.out.println("Digite o número de conta:");
         int numeroDeConta = teclado.nextInt();
+        teclado.nextLine(); // consome o \n deicao pelo next Int
 
         System.out.println("Digite o número da sua agência: ");
         String numeroAgencia = teclado.nextLine();
@@ -16,11 +17,12 @@ public class ContaTerminal {
         System.out.println("Digite seu Nome: ");
         String nomeCliente = teclado.nextLine();
 
-        System.out.print("Digite o saldo disponível:");
+        System.out.println();
+        System.out.println("Digite o saldo disponível:");
         double saldo = teclado.nextDouble();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + numeroAgencia + ", conta" + numeroDeConta + "e seu saldo " + saldo
+                + numeroAgencia + ", conta " + numeroDeConta + "e seu saldo " + saldo
                 + ", já está disponível para saque!");
 
     }
